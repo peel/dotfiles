f7886d0c4e18732aab43aec838e2d70dd210e8a4
peel <plimanowski@codearsonist.com>
2017-01-03 12:10:30 +0100

    splits aliases

--- zsh/after.zsh
+++ zsh/after.zsh
@@ -1,73 +1,10 @@
 #load custom functions
 FUNCTIONS_DIR=$HOME/.zsh.after/functions
+ALIASES_DIR=$HOME/.zsh.after/aliases
 fpath=($FUNCTIONS_DIR $fpath)
 autoload $FUNCTIONS_DIR/*(:t)
 
-#git workflow aliases
-alias gp=ghi_epic_push
-alias ghio=ghi_epic_issue_open
-alias ghios=ghi_opens
-alias ghic=ghi_close
-alias ghe=ghi_epic_open
-alias ghE=ghi_epic_close
-alias gn=ghi_nows
-alias fp=for_projects
-
-# aliases
-alias find-ips="nmap -sP $(ipconfig getifaddr en0)/24"
-alias e="emacsclient -tc"
-alias r="fc -ln -1"
-alias R="fc -ln -1"
-alias cx="chmod +x"
-
-#docker
-alias dm="docker-machine"
-alias dmi="dm ip"
-alias dme="dm env"
-alias dms="dm start && dme"
-alias dmR="dm rm default"
-alias dmc="dm create --driver virtualbox default"
-alias dc="docker-compose"
-alias dcu="dc up"
-alias dck="dc kill"
-alias dcR="dc rm -f"
-alias dckR="dck && dcR"
-alias d="docker"
-alias ds="d start"
-alias dS="d stop"
-alias dr="d restart"
-alias dl="d log"
-alias di="d images"
-alias diRu="d rmi $(docker images | grep '^<none>' | awk '{print $3}')"
-alias diR="d rmi -f $(di -aq)"
-alias dps="d ps"
-alias dex="d exec"
-
-#kubernetes
-alias k8="kubectl --context nwtprod --namespace auth"
-alias k8g="k8 get pod"
-alias k8l="k8 logs"
-alias k8lf="k8l -f"
-alias k8e="k8 exec -it"
-k8G() {
-    k8g | awk -v service="${1}-[0-9]+-.*" '$0 ~ service { print $1 }'
-}
-k8E() {
-    k8e $(k8G "$1") "$2"
-}
-k8L() {
-    k8lf $(k8G "$1")
-}
-
-#elixir
-alias ni="npm install"
-alias nib="ni && node node_modules/brunch/bin/brunch build"
-alias ism="iex -S mix"
-alias mdg="mix deps.get"
-alias mpn="mix phoenix.new"
-alias mps="mix phoenix.server"
-alias ismps="iex -S mix phoenix.server"
-alias mec="mix ecto.create"
+source $ALIASES_DIR/*
 
 export EDITOR="emacsclient -tc"
 export SHELL=/usr/local/bin/zsh
