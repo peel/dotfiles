15f6e963fd2049b0f5d383f6d0a292d40f3c3fb1
Piotr Limanowski <plimanowski@codearsonist.com>
2017-02-06 08:45:40 +0100

    Install minimal via curl

--- docs/install.sh
+++ docs/install.sh
@@ -4,7 +4,11 @@ if [ ! -d "$HOME/wrk/dotfiles" ]; then
     echo "Installing Dotfils for the first time"
     git clone --depth=1 https://github.com/peel/dotfiles.git "$HOME/wrk/dotfiles"
     cd "$HOME/wrk/dotfiles"
-    make
+    if [ "$1" = "minimal" ]; then
+        make minimal
+    else
+        make
+    fi
 else
     echo "Dotfiles already installed"
 fi
