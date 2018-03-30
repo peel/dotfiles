c5a0ae2300412b93a84a2a5df06840fa5411ca04
peel <piotr@codearsonist.com>
2018-03-30 10:02:07 +0200

    🚸 improves qmk script

--- nix/.nixpkgs/setup/common.nix
+++ nix/.nixpkgs/setup/common.nix
@@ -330,7 +330,7 @@
     r = "${pkgs.ranger}/bin/ranger";
     grep = "${pkgs.ripgrep}/bin/rg";
     alacritty = "${pkgs.alacritty}/bin/alacritty -e tmux -2 new-session -A -s main";
-    qmk = ''${pkgs.scripts}/bin/qmk /home/peel/wrk/qmk_firmware/layouts/community/ortho_4x12/peel/keymap.c'';
+    qmk = ''${pkgs.scripts}/bin/qmk $HOME/wrk/qmk_firmware/layouts/community/ortho_4x12/peel/keymap.c'';
   };
   environment.interactiveShellInit = ''
     eval "$(${pkgs.fasd}/bin/fasd --init auto)"
