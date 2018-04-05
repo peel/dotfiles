936b9598e54e093c4ce045d05308139663f61e07
peel <piotr@codearsonist.com>
2018-04-05 20:34:54 +0200

    📦 adds `emacs-plus` kind of patches

--- nix/.config/nixpkgs/overlays/peel.nix
+++ nix/.config/nixpkgs/overlays/peel.nix
@@ -5,25 +5,14 @@ self: super:
         inherit (super) callPackage stdenv fetchFromGitHub imagemagick;
         inherit (super.darwin.apple_sdk.frameworks) Carbon Cocoa ApplicationServices;
   });
-  emacs25Macport = super.stdenv.lib.overrideDerivation super.emacs25Macport (oldattrs: {
-    emacsName = "emacs-25.2-rc1";
-    name = "emacs-25.2-rc1-mac-6.2";
-    src = super.fetchurl {
-      url = "https://bitbucket.org/mituharu/emacs-mac/get/emacs-25.2-rc1-mac-6.2.tar.gz";
-      sha256 = "c379201838677834158cff57d97b045d0e837662715c90009967905c3da83648";
-    };
-    hiresSrc = super.fetchurl {
-      url = "https://s3.amazonaws.com/emacs-mac-port/Emacs.icns.modern";
-      sha256 = "eb819de2380d3e473329a4a5813fa1b4912ec284146c94f28bd24fbb79f8b2c5";
-    };
-    postPatch = ''
-      cp $hiresSrc mac/Emacs.app/Contents/Resources/Emacs.icns
-    '';
-    doCheck = false;
-    postUnpack = null;
-    macportSrc = null;
-  });
-  emacs = (if super.stdenv.isDarwin then emacs25Macport else super.emacs);
+  emacsPlus = super.callPackage ./pkgs/applications/editors/emacs {
+    with24bitColor = true;
+    withPixelScrolling = true;
+    withBorderless = true;
+    withMulticolorFonts = true;
+    withVfork = true;
+  };
+  emacs = (if super.stdenv.isDarwin then emacs else super.emacs);
   firefox-bin = super.callPackage ./pkgs/networking/browsers/firefox-bin/darwin.nix {};
   gopass = super.callPackage ./pkgs/tools/security/gopass {};
   hoverfly = super.callPackage ./pkgs/development/tools/hoverfly {};
@@ -46,6 +35,7 @@ self: super:
     inherit (super) stdenv fetchFromGitHub pkgconfig;
     inherit (super.qt5) qtbase qmake qttools qtmacextras qtx11extras;
   };
+  remacs = super.callPackage ./pkgs/applications/editors/emacs/remacs.nix {};
   rofi-emoji = super.callPackage ./pkgs/misc/rofi-emoji {};
   rofi-wifi-menu = super.callPackage ./pkgs/misc/rofi-wifi-menu {};
   scripts = super.callPackage ./pkgs/misc/scripts {
@@ -55,6 +45,5 @@ self: super:
   tmux-prompt = super.callPackage ./pkgs/misc/tmux-prompt {};
   wee-slack = super.callPackage ./pkgs/networking/weechat/wee-slack.nix {};
   zenity = super.callPackage ./pkgs/misc/zenity {};
-  remacs = super.callPackage ./pkgs/applications/editors/emacs/remacs.nix {};
 
 }
