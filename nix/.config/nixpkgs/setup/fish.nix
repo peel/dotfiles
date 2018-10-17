bdbd9aca638e48bdc0c1fb2da12c3710e0beb188
Piotr Limanowski <piotr@codearsonist.com>
2018-10-17 13:21:00 +0200

    :recycle: remove unused fish code

--- nix/.config/nixpkgs/setup/fish.nix
+++ nix/.config/nixpkgs/setup/fish.nix
@@ -102,35 +102,11 @@ in {
     if set -q TMUX; tmux setenv TMUXPWD_(tmux display -p "#D" | tr -d '%') $PWD; end
     end
   '';
-  environment.etc."fish/functions/fish_mode_prompt.fish".text = ''
-    function fish_mode_prompt --description "Display the vi mode for the prompt"
-      # Do nothing if not in vi mode
-      if test "$fish_key_bindings" = "fish_vi_key_bindings"
-        switch $fish_bind_mode
-          case default
-            set_color --bold cyan
-            echo '·'
-          case insert
-            set_color --bold cyan
-            echo '›'
-          case replace-one
-            set_color --bold red
-            echo '‹'
-          case visual
-            set_color --bold grey
-            echo '‹'
-        end
-        set_color normal
-        echo -n ' '
-      end
-    end
-  '';
   programs.fish = {
     enable = true;
     vendor.completions.enable = true;
     promptInit = ''
       source /etc/fish/functions/fish_prompt.fish
-      source /etc/fish/functions/fish_mode_prompt.fish
     '';
   };
 
