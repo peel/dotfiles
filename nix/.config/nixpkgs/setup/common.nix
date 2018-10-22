12168bfd788a59321459aa1ed0978671dcbaa554
Piotr Limanowski <piotr@codearsonist.com>
2018-10-22 13:21:00 +0200

    :recycle: remove unused configurations

--- nix/.config/nixpkgs/setup/common.nix
+++ nix/.config/nixpkgs/setup/common.nix
@@ -11,9 +11,6 @@
   services.emacs.package = pkgs.emacs;
   environment.variables.EDITOR = "${pkgs.emacs}/bin/emacsclient -tc";
   environment.variables.SHELL = "${pkgs.fish}/bin/fish";
-  environment.etc."inputrc".text = ''
-    set editing-mode vi
-  '';
   environment.etc."editorconfig".text = ''
     # top-most EditorConfig file
     root = true
@@ -65,79 +62,6 @@
       frag = magenta bold
       old = red
       new = green
-    [alias]
-      # add
-      a = add                           # add
-      chunkyadd = add --patch           # stage commits chunk by chunk
-
-      # via http://blog.apiaxle.com/post/handy-git-tips-to-stop-you-getting-fired/
-      snapshot = !git stash save "snapshot: $(date)" && git stash apply "stash@{0}"
-      snapshots = !git stash list --grep snapshot
-
-      #via http://stackoverflow.com/questions/5188320/how-can-i-get-a-list-of-git-branches-ordered-by-most-recent-commit
-      recent-branches = !git for-each-ref --count=15 --sort=-committerdate refs/heads/ --format='%(refname:short)'
-
-      # branch
-      b = branch -v                     # branch (verbose)
-
-      # commit
-      c = commit -m                     # commit with message
-      ca = commit -am                   # commit all with message
-      ci = commit                       # commit
-      amend = commit --amend            # ammend your last commit
-      ammend = commit --amend           # ammend your last commit
-
-      # checkout
-      co = checkout                     # checkout
-      nb = checkout -b                  # create and switch to a new branch (mnemonic: "git new branch branchname...")
-
-      # cherry-pick
-      cp = cherry-pick -x               # grab a change from a branch
-
-      # diff
-      d = diff                          # diff unstaged changes
-      dc = diff --cached                # diff staged changes
-      last = diff HEAD^                 # diff last committed change
-
-      # log
-      l = log --graph --date=short
-      changes = log --pretty=format:\"%h %cr %cn %Cgreen%s%Creset\" --name-status
-      short = log --pretty=format:\"%h %cr %cn %Cgreen%s%Creset\"
-      simple = log --pretty=format:\" * %s\"
-      shortnocolor = log --pretty=format:\"%h %cr %cn %s\"
-
-      # pull
-      pl = pull                         # pull
-
-      # push
-      ps = push                         # push
-
-      # rebase
-      rc = rebase --continue            # continue rebase
-      rs = rebase --skip                # skip rebase
-
-      # remote
-      r = remote -v                     # show remotes (verbose)
-
-      # reset
-      unstage = reset HEAD              # remove files from index (tracking)
-      uncommit = reset --soft HEAD^     # go back before last commit, with files in uncommitted state
-      filelog = log -u                  # show changes to a file
-      mt = mergetool                    # fire up the merge tool
-
-      # stash
-      ss = stash                        # stash changes
-      sl = stash list                   # list stashes
-      sa = stash apply                  # apply stash (restore changes)
-      sd = stash drop                   # drop stashes (destory changes)
-
-      # status
-      s = status                        # status
-      st = status                       # status
-      stat = status                     # status
-
-      # tag
-      t = tag -n                        # show tags with <n> lines of each tag message
     [format]
       pretty = format:%C(blue)%ad%Creset %C(yellow)%h%C(green)%d%Creset %C(blue)%s %C(magenta) [%an]%Creset
     [mergetool]
