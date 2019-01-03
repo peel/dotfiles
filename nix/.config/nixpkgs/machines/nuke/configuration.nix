e8014f6f4715feab1ea9f0c3ad8c2b6177a16cc5
Piotr Limanowski <piotr@codearsonist.com>
2019-01-03 15:40:17 +0100

    :sparkles: initial nuke config

---
+++ nix/.config/nixpkgs/machines/nuke/configuration.nix
@@ -0,0 +1,84 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  username = "peel";
+  hostName = "nuke";
+in {
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  nixpkgs.config.allowUnfree = true;
+  hardware.enableAllFirmware = true;
+  boot.kernelModules = [ "iwlwifi" ]; 
+  boot.loader = {
+    efi.efiSysMountPoint = "/efi";
+    grub = {
+      device = "nodev"; # do not install Grub for BIOS booting
+      efiSupport = true; 
+      extraInitrd = "/boot/initrd.keys.gz";  # add LUKS key to the initrd
+      enableCryptodisk = true; # allow Grub to boot from LUKS devices
+      zfsSupport = true;
+    };
+    grub.efiInstallAsRemovable = true;
+  };
+  boot.initrd.luks.devices.nvme = {
+   device = "/dev/disk/by-uuid/47543190-aed6-44d1-bc69-ef1f65df0bbd";
+   keyFile = "/keyfile.bin";
+  };
+
+  nix.gc.automatic = true;
+  nix.gc.options = "--delete-older-than 30d";
+
+  networking = {
+    hostId = "675e1435";
+    hostName = hostName;
+    wireless.enable = false;
+    networkmanager.enable = true;
+  };
+
+  i18n = {
+    consoleFont = "${pkgs.terminus_font}/share/consolefonts/ter-u28n.psf.gz";
+    consoleKeyMap = "us";
+    defaultLocale = "en_US.UTF-8";
+  };
+
+  environment.systemPackages = with pkgs; [ emacs ];
+
+  users.extraUsers = {
+    "${username}"= {
+      home = "/home/${username}";
+      isNormalUser = true;
+      extraGroups = [ "wheel" ];
+      uid = 1000;
+      openssh.authorizedKeys.keys = [
+        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC1hNlicWZhVgkYg6DlSs1CkjbU710RFhIc9uUFdSlk3MLtjidI0Zfbn3bfqyerXwRzOuyL7/bEuqC9lvu7b6KmvEpj1nhuOr0U1ttgIzUvgmz/cwVcVdtLv9Cr21Y2yMRDkAjj3EyLgXXZ+2sii39pYlMHcf7KFCGwgvAlHiI8ThXIW+ZbGqU6NtyoxIBPSZSbZiUz4wz02azipwFGtmGKT37sMeL635YjWqMYMJD453vPdW2F2rpq4B1NZX5L/chZs0FRytjtJJWrm1IEaqUqZXvhktKC6pDLMm/2fuqLgU8DxltXBf5vqimsJ1LPMXGOgQ0OKExiF/Gkv1Ub9x80qcEfpXninz2cvGw0xWpr7FbEShkbalt5MN7kvLIzB4Kmw71L7Af4CxcyJ0cHGGjzg8B92k1gk1rItqM8e8mGeLMrEFPJxMUCJvIE7Ywgbu7kUM8WB5x+Ni4ZmeIUHAtUUad+Z6Xd1Y7lB7190HeWWUR9+yySxOH4pUA3161sCOa5xG/mnW7QtBbD4Tqnc7ouIUq57JpSvOtilTgQAQfS3NZkNbsOv2lHDltPCqk8F4039UCxps6EbJkgNmtV31jqTiDDdQ0fCmOcl8dNygA5cTkWgMjdf1U+va68s92fVsCrwQiKm3zFNGC6VHsSAkEMc29a4d70lWCZlrcWAglZdw== piotr@codearsonist.com"
+      ];
+    };
+  };
+
+  services = {
+    openssh = {
+      enable = true;
+      permitRootLogin = "yes";
+    };
+    avahi = {
+      enable = true;
+      nssmdns = true;
+      publish.addresses = true;
+      publish.enable = true;
+      publish.workstation = true;
+      publish.domain = true;
+    };
+    xserver.enable = false;
+    nixosManual.enable = false;
+  };
+
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  system.stateVersion = "18.09";
+}
