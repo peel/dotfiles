50a95b73e70b2ebb0871270812a64468aa568b09
Piotr Limanowski <piotr@codearsonist.com>
2019-09-22 22:57:16 +0200

    updates nurpkgs

--- overlays/00-basic-pkgs.nix
+++ overlays/00-basic-pkgs.nix
@@ -4,7 +4,7 @@ let
   sources = import <dotfiles/pinned> { inherit (pkgs) fetchgit lib; };
 in {
   ghcide = import sources."ghcide-nix" {};
-  haskell-nix = import sources."haskell-nix" { inherit pkgs; };
+  haskell-nix = import sources."haskell-nix" {};
 }
 
 
--- overlays/90-apps.nix
+++ overlays/90-apps.nix
@@ -54,7 +54,7 @@ Dash = self.installApplication rec {
   sourceRoot = "Dash.app";
   src = super.fetchurl {
     url = https://kapeli.com/downloads/v4/Dash.zip;
-    sha256 = "0zvc119n30ya7xja4f2ksgqcdf8c4xjzszr8w0zgm8w65nfsi8y1";
+    sha256 = "1dkrzh4l5an0z7qv25irmagssaymscjzzgdn1jha11sqhhna9lh1";
     # date = 2018-09-28T23:36:13-0700;
   };
   description = "Dash is an API Documentation Browser and Code Snippet Manager";
@@ -78,22 +78,6 @@ Docker = self.installApplication rec {
   homepage = https://store.docker.com/editions/community/docker-ce-desktop-mac;
 };
 
-Dropbox = self.installApplication rec {
-  name = "Dropbox";
-  version = "1.0";
-  sourceRoot = "Dropbox.app";
-  src = super.fetchurl {
-    url = https://www.dropbox.com/download?plat=mac&full=1;
-    name = "Dropbox.dmg";
-    sha256 = "0mhh2lkivaykzqvc33nhz44lw5k3pbpnci01w3pjwdhxgd02x650";
-    # date = 2018-09-28T11:14:33-0700;
-  };
-  description = ''
-    Dropbox brings your files together, in one central place. They’re easy to find and safely synced across all your devices—so you can access them anytime, anywhere. No more wasting the day tracking down work.
-  '';
-  homepage = https://www.dropbox.com;
-};
-
 OnePassword = self.installApplication rec {
   name = "1Password";
   version = "7.2.1";
@@ -107,32 +91,6 @@ OnePassword = self.installApplication rec {
     Go ahead. Forget your passwords. 1Password remembers them all for you. Save your passwords and log in to sites with a single click. It's that simple.
   '';
   homepage = https://1password.com;
-};
-
-ProtonmailBridge= self.installApplication rec {
-  name = "ProtonmailBridge";
-  version = "1.0.6";
-  sourceRoot = "Protonmail\ Bridge.app";
-  src = super.fetchurl {
-    url = "https://protonmail.com/download/Bridge-Installer.dmg";
-    sha256 = "1s3cvfv6p033a696gz5h5wv8l6miw0sw0xajpz394b2d1mf9282a";
-    # date = 2018-09-28T14:23:15-0800;
-  };
-  description = "The ProtonMail Bridge is an application for paid users that runs on your computer in the background and seamlessly encrypts and decrypts your mail as it enters and leaves your computer. It allows for full integration of your ProtonMail account with any program that supports IMAP and SMTP such as Microsoft Outlook, Mozilla Thunderbird and Apple Mail.";
-  homepage = https://protonmail.com/bridge/;
-};
-
-Spotify = self.installApplication rec {
-  name = "Spotify";
-  version = "1.0.6";
-  sourceRoot = "Spotify.app";
-  src = super.fetchurl {
-    url = "https://download.scdn.co/Spotify.dmg";
-    sha256 = "0xa11xbjkp7ai4i1k0gyda1m3rl84r47977y3p880fjciffrk7rr";
-    # date = 2018-09-28T14:23:15-0800;
-  };
-  description = "Spotify is a digital music service that gives you access to millions of songs.";
-  homepage = https://spotify.com/;
 };
 
 }
