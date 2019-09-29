10f632a48b71bbd1514b1575084f7d7239c7709f
Piotr Limanowski <piotr@codearsonist.com>
2019-09-29 15:40:04 +0200

    👷‍♂️ extra lib for private files

--- overlays/00-themes/default.nix
+++ overlays/00-themes/default.nix
@@ -1,10 +1,12 @@
 self: super:
 
+with super.lib;
+
 let 
   theme = "dark";
-in 
-{
-  pragmatapro = super.callPackage ./pragmatapro {};
+in {
   colors = {} // (import ./colors.nix { });
   fonts = {} // import ./fonts.nix;
-} 
+} // optionalAttrs (files.contains "private.zip" ./pragmatapro) {
+  pragmatapro = super.callPackage ./pragmatapro {};
+}
