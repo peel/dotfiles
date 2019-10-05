5946acad14c29ee7e60b3ee17d7a8c1e1a370857
Piotr Limanowski <piotr@codearsonist.com>
2019-10-05 23:25:17 +0200

    ♻️cleanup git config

--- setup/common/git.nix
+++ setup/common/git.nix
@@ -1,4 +1,6 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
+
+with lib;
 
 let
   gitignore = pkgs.writeText "gitignore.global" ''
@@ -20,58 +22,37 @@ let
     .bloop/
     .metals/
  '';
-  gitConfig = ''
-    [user]
-      email = piotr@codearsonist.com
-      name = Piotr Limanowski
-    [github]
-      user = peel
-    [ghi]
-      token = !security find-internet-password -a peel -s github.com -l 'ghi token' -w
-    [use]
-      signingkey = piotr@codearsonist.com
-    [commit]
-      gpgsign = true
-    [color]
-      ui = true
-    [color "branch"]
-      current = yellow reverse
-      local = yellow
-      remote = green
-    [color "diff"]
-      meta = yellow bold
-      frag = magenta bold
-      old = red
-      new = green
-    [format]
-      pretty = format:%C(blue)%ad%Creset %C(yellow)%h%C(green)%d%Creset %C(blue)%s %C(magenta) [%an]%Creset
-    [mergetool]
-      prompt = false
-    [merge]
-      summary = true
-      verbosity = 1
-      tool = mvimdiff
-      ff = only
-      conflictstyle = diff3
-    [apply]
-      whitespace = nowarn
-    [branch]
-      autosetupmerge = true
-    [push]
-      # 'git push' will push the current branch to its tracking branch
-      # the usual default is to push all branches
-      default = upstream
-    [core]
-      autocrlf = false
-      excludefile = ${gitignore}
-    [advice]
-      statusHints = false
-    [diff]
-      # Git diff will use (i)ndex, (w)ork tree, (c)ommit and (o)bject
-      # instead of a/b/c/d as prefixes for patches
-      mnemonicprefix = true
-      algorithm = patience
-  '';
+  gitConfig = {
+    user = {
+      email = "piotr@codearsonist.com";
+      name = "Piotr Limanowski";
+    };
+    github.user = "peel";
+    ghi.token = "!security find-internet-password -a peel -s github.com -l 'ghi token' -w";
+    use.signingkey = "piotr@codearsonist.com";
+    commit.gpgsign = true;
+    color.ui = true;
+    format.pretty = "format:%C(blue)%ad%Creset %C(yellow)%h%C(green)%d%Creset %C(blue)%s %C(magenta) [%an]%Creset";
+    mergetool.prompt = false;
+    merge = {
+      summary = true;
+      verbosity = 1;
+      ff = "only";
+      conflictstyle = "diff3";
+    };
+    diff = {
+      mnemonicPrefix = true;
+      algorithm = "patience";
+    };
+    apply.whitespace = "nowarn";
+    branch.autosetupmerge = true;
+    push.default = "upstream";
+    core = {
+      autocrlf = true;
+      excludefile = "${gitignore}";
+    };
+    advice.statusHints = false;
+  };
 in {
   environment.systemPackages = with pkgs; [
     git
@@ -85,6 +66,6 @@ in {
     gl = "git log --pretty --graph";
   };
   
-  environment.etc."gitconfig".text = gitConfig;
+  environment.etc."gitconfig".text = generators.toINI {} gitConfig;
 
 }
