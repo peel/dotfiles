90b1ff47319fe7894ca2139d0b5467f9652b65a7
Piotr Limanowski <piotr@codearsonist.com>
2019-10-05 22:41:21 +0200

    ✨️ cocoa text system 😍️

--- setup/darwin/default.nix
+++ setup/darwin/default.nix
@@ -5,6 +5,7 @@
     ../common
     ../common/weechat.nix
     ./defaults.nix
+    ./keyboard.nix
     ./wm.nix
   ];
 
@@ -35,4 +36,35 @@
   services.nix-daemon.enable = true;
   programs.nix-index.enable = true;
 
+  peel.keybindings.mappings  = {
+    "C-l" = "centerSelectionInVisibleArea:";
+    "C-/" = "undo:";
+    "C- " = "setMark:";
+    "C-w" = "deleteToMark:";
+    "M-f" = "moveWordForward:";
+    "M-b" = "moveWordBackward:";
+    "M-<" = "moveToBeginningOfDocument:";
+    "M->" = "moveToEndOfDocument:";
+    "M-v" = "pageUp:";
+    "M-/" = "complete:";
+    "M-c" = [ "capitalizeWord:" "moveForward:" "moveForward:"];
+    "M-u" = [ "uppercaseWord:" "moveForward:" "moveForward:"];
+    "M-l" = [ "lowercaseWord:" "moveForward:" "moveForward:"];
+    "M-d" = "deleteWordForward:";
+    "C-M-h" = "deleteWordBackward:";
+    "M-Bksp" = "deleteWordBackward:";
+    "M-t" = "transposeWords:";
+    "M-\@" = ["setMark:" "moveWordForward:" "swapWithMark"];
+    "M-h" = ["setMark:" "moveToEndOfParagraph:" "swapWithMark"];
+    "C-x" = {
+      "h" = "selectAll:";
+      "k" = "performClose:";
+      "u" = "undo:";
+      "C-f" = "openDocument:";
+      "C-x" = "swapWithMark:";
+      "C-m" = "selectToMark:";
+      "C-s" = "saveDocument:";
+      "C-w" = "saveDocumentAs:";
+    };
+  };
 }
---
+++ setup/darwin/keyboard.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, lib, ...}:
+
+with lib;
+
+let
+  cfg = config.peel.keybindings;
+  mkMapping = from: to: { inherit from to; };
+  mappings = [ (mkMapping "C" "^")
+               (mkMapping "M" "~")
+               (mkMapping "S" "$")
+               (mkMapping ">" "&gt;")
+               (mkMapping "<" "&lt;")
+               (mkMapping "Bksp" "\U007F")
+               (mkMapping "-" "")];
+  replaceMappings = strings.replaceChars (builtins.map (x: x.from) mappings) (builtins.map (x: x.to) mappings);
+  format = attrsets.mapAttrs' (k: v: nameValuePair (replaceMappings k) (if attrsets.isAttrs v then format v else v));
+  keybindings = generators.toPlist {} (format cfg.mappings);
+in {
+  options.peel.keybindings = {
+    mappings = mkOption {
+      type = with types; nullOr (attrsOf (either str (either (attrsOf str) (listOf str))));
+      default = null;
+      example = ''
+       {
+         "C-/" = "undo:";
+         "C-x" = {
+            u = "undo:";
+         };
+       }
+      '';
+      description = ''
+        The text system uses a generalized key-binding mechanism that is completely re-mappable by the user, although defining custom key bindings dynamically (that is, while the application is running) is not supported. The standard key bindings are specified in /System/Library/Frameworks/AppKit.framework/Resources/StandardKeyBinding.dict. These standard bindings include a large number of Emacs-compatible control key bindings, all the various arrow key bindings, bindings for making field editors and some keyboard UI work, and backstop bindings for many function keys.
+        Details: Apple <https://developer.apple.com/library/archive/documentation/Cocoa/Conceptual/EventOverview/TextDefaultsBindings/TextDefaultsBindings.html>
+        Additional resources: Customize the Cocoa Text System <http://www.hcs.harvard.edu/~jrus/site/cocoa-text.html>
+      '';
+    };
+  };
+  
+  config = {
+    # system.activationScripts.peel.keybindings.text = mkIf (cfg.mappings != null) ''
+    system.activationScripts.postActivation.text = mkIf (cfg.mappings != null) ''
+      # Set defaults
+      echo "configuring keybindings..." >&2
+      mkdir -p ~/Library/KeyBindings || true
+      # FIXME hardling required
+      ln -fv ${pkgs.writeText "DefaultKeyBindings.dict" keybindings} ~/Library/KeyBindings/DefaultKeyBinding.dict
+     '';
+  };
+}
