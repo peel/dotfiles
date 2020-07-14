33857ecd2a3bdd2b4c29ae82a7d7c4ea6a7b0214
Piotr Limanowski <piotr@codearsonist.com>
2020-07-14 22:05:01 +0200

    :church: remove window rotation

--- overlays/20-emacs/emacs/default.el
+++ overlays/20-emacs/emacs/default.el
@@ -44,33 +44,11 @@
 (use-package windmove
   :ensure nil
   :bind (("C-|" . split-window-right)
-         ("C--" . split-window-below)
-         ("C-\\" . peel/rotate-windows))
+         ("C--" . split-window-below))
   :init
   (windmove-default-keybindings 'meta)
   :config
-  (setq windmove-wrap-around t)
-
-  (defun peel/rotate-windows (arg)
-  "Rotate your windows; use the prefix argument to rotate the other direction"
-  (interactive "P")
-  (if (not (> (count-windows) 1))
-      (message "Nope, can't rotate a single window.")
-    (let* ((rotate-times (prefix-numeric-value arg))
-           (direction (if (or (< rotate-times 0) (equal arg '(4)))
-                          'reverse 'identity)))
-      (dotimes (_ (abs rotate-times))
-        (dotimes (i (- (count-windows) 1))
-          (let* ((w1 (elt (funcall direction (window-list)) i))
-                 (w2 (elt (funcall direction (window-list)) (+ i 1)))
-                 (b1 (window-buffer w1))
-                 (b2 (window-buffer w2))
-                 (s1 (window-start w1))
-                 (s2 (window-start w2))
-                 (p1 (window-point w1))
-                 (p2 (window-point w2)))
-            (set-window-buffer-start-and-point w1 b2 s2 p2)
-            (set-window-buffer-start-and-point w2 b1 s1 p1))))))))
+  (setq windmove-wrap-around t))
 
 ;; completion ▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁▁
 (use-package company
