9c8b46af02f7e60c4fdc73c7a50cd4ea2a61fdbc
Piotr Limanowski <piotr@codearsonist.com>
2020-12-11 14:00:29 +0100

    hassio 2

--- setup/common/hassio.nix
+++ setup/common/hassio.nix
@@ -1,11 +1,24 @@
 { config, lib, pkgs, ... }:
 
 {
-  services.home-assistant = {
-    enable = true;
-    applyDefaultConfig = true;
-    configWritable = true;
-    openFirewall = true;
-    autoExtraComponents = true;
+  virtualisation.oci-containers.containers = {
+    home-assistant = {
+      autoStart = true;
+      image = "home-assistant/home-assistant:1.0.0b5";
+      environment = {
+        TZ="Europe/Warsaw";
+      };
+      ports = [
+        "8123:8123"
+      ];
+      volumes = [
+        "/home/peel/wrk/hassiol:/config"
+        "/etc/localtime:/etc/localtime"
+      ];
+      extraOptions = [
+        "--network=host"
+        "--device=/dev/ttyACM0:/dev/ttyACM0"
+      ];
+    };
   };
 }
