4fa7c6578e50cfb197393fd8f40ecc8e7f9fae65
Piotr Limanowski <piotr@codearsonist.com>
2022-02-01 01:05:03 +0100

    add rofi

--- setup/common/config.hs
+++ setup/common/config.hs
@@ -1,10 +1,15 @@
 import XMonad
 import XMonad.Util.SpawnOnce
+import XMonad.Util.EZConfig(additionalKeys)
 
 myStartupHook = do
   spawnOnce "emacsclient -c"
 
+myLauncher = "rofi -combi-modi window,drun,run -show combi -modi combi"
+
 main = xmonad defaultConfig
         { terminal = "emacsclient -c --eval '(vterm)'",
           startupHook = myStartupHook
-        }
+        } `additionalKeys`
+        [ ((modMask, xK_p ), spawn myLauncher)
+        ]
