006bb0f46b2798cbed8fd63129087543ed82249a
Piotr Limanowski <plimanowski@pm.me>
2022-02-09 14:13:48 +0100

    common: improve font rendering on nixos

--- modules/common/setup/fonts.nix
+++ modules/common/setup/fonts.nix
@@ -7,8 +7,10 @@
   } // lib.optionalAttrs pkgs.stdenvNoCC.isLinux {
     fontconfig.enable = true;
     enableDefaultFonts = false;
-    fontconfig.defaultFonts.monospace = ["PragmataPro"];
-    fontconfig.defaultFonts.sansSerif = ["PragmataPro"];
-    fontconfig.defaultFonts.serif = ["PragmataPro"];
+    fontconfig.defaultFonts.monospace = ["PragmataPro Liga"];
+    fontconfig.defaultFonts.sansSerif = ["PragmataPro Liga"];
+    fontconfig.defaultFonts.serif = ["PragmataPro Liga"];
+    fontconfig.hinting.enable = false;
+    fontconfig.antialias = true;
   };
 }
